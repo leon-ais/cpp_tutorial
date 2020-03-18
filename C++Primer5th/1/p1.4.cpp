28d1f6fa9af75eaf5b284768af4e8b1a870f524b
leon-ais <b15010820@njupt.edu.cn>
2020-03-19 07:09:59 +0800

    C++Primer

---
+++ C++Primer5th/1/p1.4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// 声明乘法做积的函数 
+int production(int num1, int num2);
+
+int main() {
+	int a = 0, b = 0;
+	cout << "please input two number:" << endl;
+	cin >> a >> b ;
+	cout << "the result of two number is:" << production(a, b) << endl;
+	return 0;
+}
+
+// 求两数之积 
+int production(int elem1, int elem2) {
+	return elem1 * elem2;
+}
+
